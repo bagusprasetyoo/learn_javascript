cbeaa3d905034913ea728f837479546af754e1bd
bagusprasetyoo <bagusprstyoo11@gmail.com>
2024-11-21 10:35:24 +0800

    Mengenal alert, console.log, dan prompt

---
+++ main.js
@@ -0,0 +1,9 @@
+/* ini alert untuk memunculkan pop up pada browser */
+// alert('ini namanya alert')
+
+/* ini console.log untuk memunculkan isi pada console */
+/* berfungsi sebagai trouble shooter atau debugging */
+// console.log('ini namanya console log')
+
+/* ini prompt untuk memunculkan pop up berupa inputan */
+// prompt('ini namanya prompt memunculkan pop up dengan inputan')
